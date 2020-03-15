7f07f27a011808db6474c060524834f22238ac9b
EmildaZhang <emildazhg07@gmail.com>
2020-03-16 00:02:45 +0700

    adding client side

---
+++ client/src/pages/StudentList/StudentList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StudentList(props) {
+  return (
+    <div className="student container">
+      HELLO THIS IS MY FISRT WEBPACK SERIES
+    </div>
+  );
+}
+
+export default StudentList;
